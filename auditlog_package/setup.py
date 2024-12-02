4881fc9050b7752aa9d1872074def2ffb55c0a11
EC2 Default User <ec2-user@ip-172-31-11-218.ec2.internal>
2024-12-02 10:40:20 +0000

    manifest

--- auditlog_package/setup.py
+++ auditlog_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='audit-trail-logger',
-    version='0.1.1',
+    version='0.1.2',
     description='A Django package for logging actions with user and object details',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
